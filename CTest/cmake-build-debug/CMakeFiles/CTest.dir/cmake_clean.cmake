ff87d4d86c1ff2d24781a4de6c4897c2b7cffd57
jiaquanxia <913882743@qq.com>
2021-06-24 10:16:19 +0800

    *CTest:CLion第一个C测试程序

---
+++ CTest/cmake-build-debug/CMakeFiles/CTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CTest.dir/main.c.obj"
+  "CTest.exe"
+  "CTest.exe.manifest"
+  "CTest.pdb"
+  "libCTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 2021CLionPro
 2021CLionPro
+1.CTest
+CLion第一个C测试程序
