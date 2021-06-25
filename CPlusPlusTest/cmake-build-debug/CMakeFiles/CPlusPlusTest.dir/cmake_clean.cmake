295004af5096bd3fa36831433e93d049595bda46
jiaquanxia <ejiaquan81@gmail.com>
2021-06-25 21:56:46 +0800

    *CPlusPlusTest:函数模板的使用

---
+++ CPlusPlusTest/cmake-build-debug/CMakeFiles/CPlusPlusTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlusPlusTest.dir/main.cpp.obj"
+  "CPlusPlusTest.exe"
+  "CPlusPlusTest.exe.manifest"
+  "CPlusPlusTest.pdb"
+  "libCPlusPlusTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlusPlusTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
